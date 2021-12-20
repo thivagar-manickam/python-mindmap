d75cef2a762b13686c25d3f96e3de8934e8f8956
thivagar-manickam <thivagar200@gmail.com>
2021-12-20 22:56:26 +0530

    Updates

---
+++ precache-manifest.db8681dba00279b8cfa277dfc9f7f773.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e9acaa02070cd025c6cef8126cfa7f4",
+    "url": "/thivagar-manickam/python-mindmap/index.html"
+  },
+  {
+    "revision": "0a8c7fc5fdcc4d48ad02",
+    "url": "/thivagar-manickam/python-mindmap/static/css/chunk-blinkmind.0c51329f.chunk.css"
+  },
+  {
+    "revision": "e99b8dd18498f0d38c0b",
+    "url": "/thivagar-manickam/python-mindmap/static/css/chunk-libs.add42989.chunk.css"
+  },
+  {
+    "revision": "0a8c7fc5fdcc4d48ad02",
+    "url": "/thivagar-manickam/python-mindmap/static/js/chunk-blinkmind.9a29a633.chunk.js"
+  },
+  {
+    "revision": "e99b8dd18498f0d38c0b",
+    "url": "/thivagar-manickam/python-mindmap/static/js/chunk-libs.61c00e73.chunk.js"
+  },
+  {
+    "revision": "e6068e20c2cf1b882c52",
+    "url": "/thivagar-manickam/python-mindmap/static/js/chunk-slate.393dec83.chunk.js"
+  },
+  {
+    "revision": "5ce1b6d9a49b7b2f0a9f",
+    "url": "/thivagar-manickam/python-mindmap/static/js/main.5cb82992.chunk.js"
+  },
+  {
+    "revision": "327a6788e3821e799bc1",
+    "url": "/thivagar-manickam/python-mindmap/static/js/runtime~main.2e8ee811.js"
+  },
+  {
+    "revision": "4f8c49830f9d89080c0e14ef27fe5119",
+    "url": "/thivagar-manickam/python-mindmap/static/media/default.4f8c4983.jpg"
+  },
+  {
+    "revision": "1615fe237931cc9888e8da84ee2bddf9",
+    "url": "/thivagar-manickam/python-mindmap/static/media/iconfont.1615fe23.ttf"
+  },
+  {
+    "revision": "39b55534800824e0e0fcbc8613765f72",
+    "url": "/thivagar-manickam/python-mindmap/static/media/iconfont.39b55534.svg"
+  },
+  {
+    "revision": "3ea7cc77255df2a8a4203699c5c7ba8c",
+    "url": "/thivagar-manickam/python-mindmap/static/media/iconfont.3ea7cc77.ttf"
+  },
+  {
+    "revision": "46a2ccc0f9b5e741b0a7236a45595cfc",
+    "url": "/thivagar-manickam/python-mindmap/static/media/iconfont.46a2ccc0.svg"
+  },
+  {
+    "revision": "48fe2018c6493d7b6dcad333f63778e5",
+    "url": "/thivagar-manickam/python-mindmap/static/media/iconfont.48fe2018.woff"
+  },
+  {
+    "revision": "52b6ee751b76bedccfefc9862ac73b8c",
+    "url": "/thivagar-manickam/python-mindmap/static/media/iconfont.52b6ee75.woff"
+  },
+  {
+    "revision": "aa15821e33931d66ea8a44e325c97faf",
+    "url": "/thivagar-manickam/python-mindmap/static/media/iconfont.aa15821e.eot"
+  },
+  {
+    "revision": "c7f498034eb3b56e4545ebd2891aacc0",
+    "url": "/thivagar-manickam/python-mindmap/static/media/iconfont.c7f49803.eot"
+  },
+  {
+    "revision": "2f59f00b1baa3f74747694eff9e53875",
+    "url": "/thivagar-manickam/python-mindmap/static/media/theme1.2f59f00b.jpg"
+  },
+  {
+    "revision": "b18193fbba175e35f6fbf3391f385723",
+    "url": "/thivagar-manickam/python-mindmap/static/media/theme2.b18193fb.jpg"
+  },
+  {
+    "revision": "61c97910ac792963391befcf87c7ce00",
+    "url": "/thivagar-manickam/python-mindmap/static/media/theme3.61c97910.jpg"
+  },
+  {
+    "revision": "1e21408c23fc4c6933552278be804add",
+    "url": "/thivagar-manickam/python-mindmap/static/media/theme4.1e21408c.jpg"
+  }
+]);
